47ff77910e91c8ad6fea8c1b9b98444db0375398
BloodAide <365809476@qq.com>
2018-07-18 15:07:17 +0800

    aaaa

---
+++ gitTest2.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class gitTest {
+	public void test1() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("wwwwwwwwwwwwww");
+		}
+	}
+	public void test2() {
+		
+                        System.out.println("wwww213123123wwwwwwwwww");
+			System.out.println("wwwwwwwwwwwwww");
+		
+	}
+}
